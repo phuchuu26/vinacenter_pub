aec2fb80b49051d303af09ed8a1cd59cab329ad7
phucnh61 <phucnh61@fpt.com.vn>
2023-01-10 21:37:27 +0700

    complete thong ke

--- app/Http/Controllers/Admin/ChartSaleController.php
+++ app/Http/Controllers/Admin/ChartSaleController.php
@@ -10,6 +10,9 @@ use App\Models\OrderProduct;
 use App\Models\Statics;
 use App\Models\User;
 use DateTime;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ChartSaleController extends Controller
 {
@@ -20,6 +23,8 @@ class ChartSaleController extends Controller
     	$data['best_saler'] = $this->getBestSaler();
     	$data['product_sale'] = $this->getProductSale();
     	$data['sale'] = $this->getSale();
+    	$data['list'] = $this->getListEmployees();
+        // dd(	$data['list'] );
         return view('admin.module.chart_sale.view',['data' => $data]);
     }
 
@@ -50,6 +55,8 @@ class ChartSaleController extends Controller
         left JOIN product_option AS v ON o.product_id = v.id
         GROUP BY product_id , v.name
         ORDER BY qty DESC");
+
+
         return $data[0];
        
     }
@@ -79,5 +86,76 @@ class ChartSaleController extends Controller
         return $data;
        
     }
+
+    
+    public function getListEmployees()
+    {
+        
+        $data = []; 
+
+        $week = \DB::select(" 			select  sum(REPLACE(total, ',', '')) as sale, user_id
+        FROM order_product
+        WHERE created_at  between DATE_SUB(CURDATE(), INTERVAL 7 DAY) and DATE_ADD(CURDATE(), INTERVAL 1 DAY)
+        GROUP BY user_id
+        ORDER BY sale DESC
+        ");
+
+        
+
+        $month = \DB::select(" 
+            select  sum(REPLACE(total, ',', '')) as sale, user_id
+            FROM order_product
+            WHERE created_at  between DATE_SUB(CURDATE(), INTERVAL 1 MONTH) and DATE_ADD(CURDATE(), INTERVAL 1 DAY)
+            GROUP BY user_id
+            ORDER BY sale DESC
+        ");
+        
+        $year = \DB::select("  		select  sum(REPLACE(total, ',', '')) as sale, user_id
+        FROM order_product
+        WHERE created_at  between DATE_SUB(CURDATE(), INTERVAL 1 YEAR) and DATE_ADD(CURDATE(), INTERVAL 1 DAY)
+        GROUP BY user_id
+				ORDER BY sale DESC
+        ");
+
+        
+        foreach($year as &$val){
+            $user = User::where('username', data_get($val, 'user_id'))->first();
+            $val->name = data_get($user, 'name') ?? ''; 
+            $val->id = data_get($user, 'id') ?? ''; 
+            $val->created_at = data_get($user, 'created_at') ?? ''; 
+        }
+
+        foreach($month as &$val){
+            $user = User::where('username', data_get($val, 'user_id'))->first();
+            $val->name = data_get($user, 'name') ?? ''; 
+            $val->id = data_get($user, 'id') ?? ''; 
+            $val->created_at = data_get($user, 'created_at') ?? ''; 
+        }
+      
+        foreach($week as &$val){
+            $user = User::where('username', data_get($val, 'user_id'))->first();
+            $val->name = data_get($user, 'name') ?? ''; 
+            $val->id = data_get($user, 'id') ?? ''; 
    
+            $val->created_at = data_get($user, 'created_at') ?? ''; 
+        }
+        $month = $this->paginate($month);
+        $year = $this->paginate($year);
+        $week = $this->paginate($week);
+        
+        // dd( $month );
+        $data['week'] =   $week;
+        $data['month'] =   $month;
+        $data['year'] =   $year;
+        return $data;
+       
+    }
+
+    public function paginate($items, $perPage = 10, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        $options['path'] = route('getChartSale');
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
 }
--- resources/views/admin/module/chart_sale/view.blade.php
+++ resources/views/admin/module/chart_sale/view.blade.php
@@ -3,6 +3,11 @@
 @extends('admin.master')
 @section('title','Cập nhật thông tin tài khoản')
 @section('content')
+<style>
+    p {
+    font-weight: bold;
+}
+</style>
     <div class="table-agile-info">
         <div class="panel panel-default">
             <form action="{{route('login.update_info_user')}}" method="POST">
@@ -107,7 +112,213 @@
                     <br>
                     <br>
 
+                    <div class="col-lg-12">  
+                        <div class="input-group mb-3">
+                            <p> &nbsp&nbsp&nbsp&nbsp Danh sách doanh thu Nhân viên:</p> 
+                        </div>
+                    </div>
+
+
+
+
+                        
+
+
+                        <div class="col-lg-12">
+                            <table class="table table-hover">
+                                <thead>
+                                <tr>
+                                    <th colspan="10"><h6>Danh sách doanh thu nhân viên của tuần</h6></th>
+                                </tr>
+                                <tr class="list_heading">
+                                    <th>STT</th>
+                                    <th>Tên nhân viên</th>
+                                    <th>Username</th>
+                                    <th>Doanh thu</th>
+                                    <th>Thời gian tạo tài khoản</th>
+                                    <th class="action_col" colspan="2"></th>
+                                </tr>
+                                </thead>
+                                @php
+                                    $stt =$data['list']['week']->firstItem();
+                                @endphp
+                                @if(!empty($data['list']['week']))
+                                @foreach($data['list']['week'] as $item)
+                                    
+                                        <tr class="list_data">
+                                            <td class="list_td aligncenter">{!!  $stt !!}</td>
+                                            <td class="list_td aligncenter">{!! $item->name !!}</td>
+                                            <td class="list_td aligncenter">{!! $item->user_id !!}</td>
+                    
+                                            <td class="list_td aligncenter"> 
+                                                 {!! number_format(  (int) data_get( $item, 'sale') ) .' đ' !!}
+                                                </td>
+                                        
+                                            <td class="list_td aligncenter">
+                                                <?php \Carbon\Carbon::setLocale('vi'); ?>
+                                                {!! \Carbon\Carbon::createFromTimeStamp(strtotime($item->created_at))->diffForHumans() !!}
+                                            </td>
+                                       
+                
+                                            <td class="list_td aligncenter">
+                                                <a style="    max-width: 70px; font-size: 10px;" type="button"
+                                                    class="btn btn-info" href="{{route('getUserView', ['id' =>  data_get( $item, 'id') ])}}"> 
+                                                    
+                                                    Thông tin nhân viên
+                                                </a>&nbsp;&nbsp;&nbsp;
+                                           </td>
+    
+                                        </tr>
+                                        @php
+                                        ++$stt;
+                                        // dd( $item);
+                                        @endphp
+                                    @endforeach
+                                    @endif
+                                </table>
+                            </div>
+                            @if(count($data['list']['week']) > 0)
+                                <div class="col-lg-12">
+                                    <div id="paging_nav">{{ $data['list']['week']->render() }}</div>
+                                </div>
+                            @endif
+        
+<hr>
+        
+<br>
+<br>
+<br>
+
+                        
+
+                    <div class="col-lg-12">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th colspan="10"><h6>Danh sách doanh thu nhân viên của tháng</h6></th>
+                            </tr>
+                            <tr class="list_heading">
+                                <th>STT</th>
+                                <th>Tên nhân viên</th>
+                                <th>Username</th>
+                                <th>Doanh thu</th>
+                                <th>Thời gian tạo tài khoản</th>
+                                <th class="action_col" colspan="2"></th>
+                            </tr>
+                            </thead>
+                            @php
+                                $stt =$data['list']['month']->firstItem();
+                            @endphp
+                            @if(!empty($data['list']['month']))
+                            @foreach($data['list']['month'] as $item)
+                                
+                                    <tr class="list_data">
+                                        <td class="list_td aligncenter">{!!  $stt !!}</td>
+                                        <td class="list_td aligncenter">{!! $item->name !!}</td>
+                                        <td class="list_td aligncenter">{!! $item->user_id !!}</td>
+                
+                                        <td class="list_td aligncenter"> 
+                                             {!! number_format(  (int) data_get( $item, 'sale') ) .' đ' !!}
+                                            </td>
+                                    
+                                        <td class="list_td aligncenter">
+                                            <?php \Carbon\Carbon::setLocale('vi'); ?>
+                                            {!! \Carbon\Carbon::createFromTimeStamp(strtotime($item->created_at))->diffForHumans() !!}
+                                        </td>
+                                   
+            
+                                        <td class="list_td aligncenter">
+                                            <a style="    max-width: 70px; font-size: 10px;" type="button"
+                                                class="btn btn-info" href="{{route('getUserView', ['id' =>  data_get( $item, 'id') ])}}"> 
+                                                
+                                                Thông tin nhân viên
+                                            </a>&nbsp;&nbsp;&nbsp;
+                                       </td>
+
+                                    </tr>
+                                    @php
+                                    ++$stt;
+                                    // dd( $item);
+                                    @endphp
+                                @endforeach
+                                @endif
+                            </table>
+                        </div>
+                        @if(count($data['list']['month']) > 0)
+                            <div class="col-lg-12">
+                                <div id="paging_nav">{{ $data['list']['month']->render() }}</div>
+                            </div>
+                        @endif
+
+
+<hr>
+        
+<br>
+<br>
+<br>
+
+
+
+<div class="col-lg-12">
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th colspan="10"><h6>Danh sách doanh thu nhân viên của năm</h6></th>
+        </tr>
+        <tr class="list_heading">
+            <th>STT</th>
+            <th>Tên nhân viên</th>
+            <th>Username</th>
+            <th>Doanh thu</th>
+            <th>Thời gian tạo tài khoản</th>
+            <th class="action_col" colspan="2"></th>
+        </tr>
+        </thead>
+        @php
+            $stt =$data['list']['year']->firstItem();
+        @endphp
+        @if(!empty($data['list']['year']))
+        @foreach($data['list']['year'] as $item)
+            
+                <tr class="list_data">
+                    <td class="list_td aligncenter">{!!  $stt !!}</td>
+                    <td class="list_td aligncenter">{!! $item->name !!}</td>
+                    <td class="list_td aligncenter">{!! $item->user_id !!}</td>
+
+                    <td class="list_td aligncenter"> 
+                         {!! number_format(  (int) data_get( $item, 'sale') ) .' đ' !!}
+                        </td>
+                
+                    <td class="list_td aligncenter">
+                        <?php \Carbon\Carbon::setLocale('vi'); ?>
+                        {!! \Carbon\Carbon::createFromTimeStamp(strtotime($item->created_at))->diffForHumans() !!}
+                    </td>
                
+
+                    <td class="list_td aligncenter">
+                        <a style="    max-width: 70px; font-size: 10px;" type="button"
+                            class="btn btn-info" href="{{route('getUserView', ['id' =>  data_get( $item, 'id') ])}}"> 
+                            
+                            Thông tin nhân viên
+                        </a>&nbsp;&nbsp;&nbsp;
+                   </td>
+
+                </tr>
+                @php
+                ++$stt;
+                // dd( $item);
+                @endphp
+            @endforeach
+            @endif
+        </table>
+    </div>
+    @if(count($data['list']['year']) > 0)
+        <div class="col-lg-12">
+            <div id="paging_nav">{{ $data['list']['year']->render() }}</div>
+        </div>
+    @endif
+                       
+            
 
                 
                
